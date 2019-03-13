1d2fcbeaed488e4b24cbe1766356812e0f9df159
nametae2 <48530862+nametae2@users.noreply.github.com>
2019-03-14 02:01:02 +0700

    Update AutoEyeInfractionEvent.java

--- src/main/java/com/heirteir/autoeye/api/AutoEyeInfractionEvent.java
+++ src/main/java/com/heirteir/autoeye/api/AutoEyeInfractionEvent.java
@@ -21,7 +21,6 @@ import org.bukkit.event.HandlerList;
     private static final HandlerList handlerList = new HandlerList();
     private final Player player;
     private final InfractionData.Infraction infraction;
-    private String message = "&e%player%&7 was caught using &e%check%&7. (VL=&e%vl%&7)";
     private boolean cancelled = false;
 
     public String getMessage() {
